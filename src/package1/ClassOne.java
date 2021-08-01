cb4eb4d42f292de687c97bdded7a3f48e0278745
AykutS <gulerrahim@gmail.com>
2021-08-01 15:40:20 -0400

    second commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -4,6 +4,7 @@ public class ClassOne {
     public static void main(String[] args) {
 
         system.out.println("Hello World");
+        System.out.println("Batch 35/36 lear GitHub");
 
     }
 }
