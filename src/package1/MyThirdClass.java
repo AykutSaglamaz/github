dd49c8b8c0508b65ea5f93bf0f932597dad1b3c0
AykutS <gulerrahim@gmail.com>
2021-08-07 15:04:54 -0400

    new version

--- src/package1/MyThirdClass.java
+++ src/package1/MyThirdClass.java
@@ -7,7 +7,25 @@ public class MyThirdClass {
         System.out.println(sum);
         
          int y =0;
-      System.out.println("This is remove REPO CODE");   
+      System.out.println("This is remove REPO CODE");
+
+//        --GIT PULL--
+//        1. Create new commit in REMOTE REPOSITORY
+//        2. Go to MyThirdClass.java
+//        3. Click on pencil symbol
+//        4. Add new codes
+//        5. scroll down
+//        6. Click commit changes
+//                --------
+//        These instructions are to create new commit(version) in the remote repository
+//        ------
+//                Imagine these code can be my own code or my friends code
+//                ------
+//        As of now GitHub has the latest code
+//        This means  I have to pull that code, OTHERWISE I CANNOT PUSH THE CODE TO GITHUB. Because github will throw error
+//                -----
+//                As a best practice, Do the following
+//        git pull BEFORE every git push
         
     }
 }
